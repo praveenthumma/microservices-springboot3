f79df1ce6520a7476b2f34c28513a6342630aaf3
Praveen Thumma <praveen.thumma22@gmail.com>
2024-05-12 23:09:41 +0530

    corrected syntax

---
+++ inventory-service/src/main/java/com/tp/ms/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,33 @@
+package com.tp.ms.inventoryservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tp.ms.inventoryservice.service.InventoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.OK)
+    public String isInStock() {
+        return "Service is UP";
+    }
+    
+}
