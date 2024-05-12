4241d4b7bf841a9f4e9a5c891bfa488bd343fb76
Praveen Thumma <praveen.thumma22@gmail.com>
2024-05-12 22:00:01 +0530

    added invenotry files

---
+++ inventory-service/src/main/java/com/tp/ms/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.tp.ms.inventoryservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tp.ms.inventoryservice.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long>{
+
+    
+    
+}
