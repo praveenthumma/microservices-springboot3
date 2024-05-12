5e12fff2eb2a1b03f51db7ed013e3b03d56108c6
Praveen Thumma <praveen.thumma22@gmail.com>
2024-05-12 23:15:11 +0530

    corrected syntax

--- inventory-service/src/main/java/com/tp/ms/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/tp/ms/inventoryservice/repository/InventoryRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tp.ms.inventoryservice.model.Inventory;
 
-public interface InventoryRepository extends JpaRepository<Inventory,Long>{
-
-    
-    
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    public boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
 }
